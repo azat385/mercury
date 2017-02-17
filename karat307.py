88284253fe6d45c5de5a04364d5442d9bb54546b
ews <ews@mail.ru>
2017-02-17 18:44:43 +0300

    karat 307 mod

--- karat307.py
+++ karat307.py
@@ -1,30 +1,38 @@
 # -*- coding: utf-8 -*-
-from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
-client = ModbusClient(method="rtu", port="/dev/ttyUSB1",baudrate=9600,timeout=2)
-client.connect()
+import sys
 
-rr = client.read_holding_registers(0x0062,4, unit=0x03)
-print [hex(i) for i in rr.registers]
-d = rr.registers
-
-client.close()
-
-
-from struct import pack, unpack
-
-data_list = unpack('<6BH', pack('>4H',*d))
-
-
-from pymodbus.client.sync import ModbusTcpClient
 from collections import namedtuple
 A = namedtuple('A', 'ip port id start_address')
+
+if len(sys.argv) >= 2:
+    address = int(sys.argv[1])
+    start_address = int(sys.argv[2])
+else:
+    address = 0x03
+    start_address = 400
+
 client_settings = A(ip='192.168.55.4',
                     port=8000,
                     id=0x01,
-                    start_address=400)
+                    start_address=start_address)
+
+# rtu master
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+client = ModbusClient(method="rtu", port="/dev/ttyUSB1",baudrate=9600,timeout=2)
+client.connect()
+
+rr = client.read_holding_registers(0x0062,4, unit=address)
+print [hex(i) for i in rr.registers]
+d = rr.registers
+client.close()
+
+# transform the data
+from struct import pack, unpack
+data_list = unpack('<6BH', pack('>4H',*d))
 
 # tcp client
+from pymodbus.client.sync import ModbusTcpClient
 TCPclient = ModbusTcpClient(host=client_settings.ip, port=client_settings.port)
 
 rq = TCPclient.write_registers(client_settings.start_address, data_list, unit=client_settings.id)
